222836634323ec4ede9fcd8980a07fa1242a6cc9
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2023-01-12 14:28:18 +0100

    Footer section completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
 import { NavBar } from './NavBar';
 import Hero from './Hero';
 import Processes from './Processes';
 import Services from './Services';
 import ProductStories from './ProductStories';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="container mx-auto bg-[#F5F4F0] [&_*]:transition-all [&_*]:duration-200 [&_*]:ease-linear">
+    <div className="container mx-auto bg-[#F5F4F0] scroll-smooth [&_*]:transition-all [&_*]:duration-200 [&_*]:ease-linear">
         <NavBar></NavBar>
         <Hero />
         <Services />
         <Processes />
         <ProductStories />
+        <Footer />
     </div>
   );
 }
